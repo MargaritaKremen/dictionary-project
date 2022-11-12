f389d6dbd13222dfb15817b2597617dcd39653a8
MargaritaKremen <53299362+MargaritaKremen@users.noreply.github.com>
2022-11-12 13:35:00 +0000

    project initialization

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="text-center">
+       
+       hello
+      </header>
+    </div>
+  );
+}
+
+ 
