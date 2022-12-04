279787ecfe3b76824dc23e1b4cc18decd77dcbef
MargaritaKremen <53299362+MargaritaKremen@users.noreply.github.com>
2022-12-04 16:02:49 +0000

    developed by...

--- src/App.js
+++ src/App.js
@@ -11,7 +11,32 @@ export default function App() {
         </main>
       </header>
       <footer>
-        Developed by Margarita Kozlova
+      This project was coded by{" "}
+        <a
+          href="https://linkedin.com/in/margarita-kozlova"
+          target="_blank"
+          rel="noreferrer"
+        >
+         Margarita Kozlova 
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/MargaritaKremen/dictionary-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://elegant-valkyrie-a26e75.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+        .
+        
         </footer>
     </div>
   );
